17828f14057f37de57f6867a06391132bb8b4dea
Chloé Laugier <laugier.chloe@gmail.com>
2015-06-01 16:42:47 +0200

    Initial Commit

---
+++ WeatherToday/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WeatherToday
+//
+//  Created by Chloé Laugier on 01/06/2015.
+//  Copyright (c) 2015 Chloé Laugier. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
